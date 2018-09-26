9d67268b54fb5c7ff1ab96233e4b404740e150d4
杨帆 <42921163+609980332@users.noreply.github.com>
2018-09-26 11:07:59 +0800

    Update AdminService.java

--- src/main/java/cap/service/AdminService.java
+++ src/main/java/cap/service/AdminService.java
@@ -1,5 +1,19 @@
+<!--杨帆-->
+
 package cap.service;
 
-public class AdminService {
-}
+import cap.mapper.AdminMapper;
+import cap.model.Admin;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
+@Service
+public class AdminService {
+    @Resource
+    private AdminMapper adminMapper;
+
+    public Admin login(Admin admin){
+        return adminMapper.selectBySelective(admin);
+    }
+}
